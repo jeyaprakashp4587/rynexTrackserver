dbfa4bd3ca7b37b4d4d959d25c3f12e7382f2cf5
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2026-07-22 23:26:48 +0530

    --update rust backend

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod app;
+mod infrastructure;
+mod routes;
+mod state;
+
+#[tokio::main]
+
+async fn main () {
+    let app = app::build().await;
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    println!("🚀 Server running on http://localhost:3000");
+
+    axum::serve(listener, app).await.unwrap();
 }
