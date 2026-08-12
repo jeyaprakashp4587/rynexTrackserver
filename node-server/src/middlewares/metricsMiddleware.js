e46408cdc2c86534934930847c4e2ed77a290b0c
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2026-08-12 22:10:56 +0530

    --config : metrics

--- node-server/src/middlewares/metricsMiddleware.js
+++ node-server/src/middlewares/metricsMiddleware.js
@@ -2,11 +2,14 @@ import app from "../app";
 import { httpRequestDuration } from "../Monitoring/PrometheusMetrics";
 
 app.use((req, res, next) => {
+  if (req.path === "/metrics") {
+    return next();
+  }
+
   const start = process.hrtime();
 
   res.on("finish", () => {
     const diff = process.hrtime(start);
-
     const duration = diff[0] + diff[1] / 1e9;
 
     httpRequestDuration
