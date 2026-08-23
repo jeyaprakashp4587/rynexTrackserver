56e3b9fb5fa000926ba88243b102db9ddb5eb65d
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2026-08-24 00:02:52 +0530

    --docker: update

--- node-server/src/config/socket.config.js
+++ node-server/src/config/socket.config.js
@@ -3,10 +3,7 @@ import { Server } from "socket.io";
 let io;
 
 export const initSocket = (server) => {
-  // console.log("socket server", server);
-
   io = new Server(server, {
-
     cors: {
       origin: "*",
       allowedHeaders: ["Authorization", "Content-Type"],
