50d26c934ef6a585803cc9edf9bfe8059d87f581
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2026-09-17 23:30:34 +0530

    --update: fix rust

---
+++ rust-liveTracking-server/src/app.rs
@@ -0,0 +1,12 @@
+// use axum::{
+//     routing::get,Router
+// };
+
+// use crate::{
+//     routes,
+//     state::AppState
+// }:
+
+// pub fn create_app(state: AppState) -> Router {
+//     Router::new().router(routes::routes(state))
+// }
--- rust-liveTracking-server/src/main.rs
+++ rust-liveTracking-server/src/main.rs
@@ -5,5 +5,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
     println!("Server listening on 127.0.0.1:8080");
     Ok(())
-    // update
+  
 }
